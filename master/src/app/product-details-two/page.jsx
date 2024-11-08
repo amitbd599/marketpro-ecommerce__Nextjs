e9f06e18e730a2ebee1ddc7c3320797726b0abdb
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-11-08 20:21:25 +0600

    HeaderTwo add

---
+++ master/src/app/product-details-two/page.jsx
@@ -0,0 +1,30 @@
+import HeaderTwo from "@/components/HeaderTwo";
+import ColorInit from "@/helper/ColorInit";
+import Preloader from "@/helper/Preloader";
+import ScrollToTopInit from "@/helper/ScrollToTopInit";
+
+export const metadata = {
+  title: "MarketPro - E-commerce Next JS Template",
+  description:
+    "MarketPro is a comprehensive and versatile Next JS template designed for e-commerce platforms, specifically tailored for multi vendor marketplaces. With its modern design and extensive feature set, MarketPro provides everything you need to create a robust and user-friendly online marketplace..",
+};
+
+const page = () => {
+  return (
+    <>
+      {/* ColorInit */}
+      <ColorInit color={true} />
+
+      {/* ScrollToTop */}
+      <ScrollToTopInit />
+
+      {/* Preloader */}
+      <Preloader />
+
+      {/* HeaderTwo */}
+      <HeaderTwo category={true} />
+    </>
+  );
+};
+
+export default page;
